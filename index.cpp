885c55a8e6c8326a1986681ecfb7bdfa9c1326c5
m93a <m93a.cz@gmail.com>
2014-04-25 20:40:30 +0200

    stacking
    particle array
    known bugs: Sigswag the Murderer kills this process, probably stack overflow...

--- index.cpp
+++ index.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <string>
 #include <stdlib.h>
-#include <math.h>
+#include <cmath>
 #include <GL/glut.h>
 //#include <windows.h>  //pro wokna
 #include <X11/Xlib.h> //pro X11
@@ -14,6 +14,9 @@ using namespace std;
 /* * * * * * * *
  * helpers.cpp *
  * * * * * * * */
+int    pow2(int x   ){return x*x;}
+float  pow2(float x ){return x*x;}
+double pow2(double x){return x*x;}
 void polygon(
  int n,
  float x,
@@ -65,35 +68,79 @@ class Vector {
   Vector(float x, float y){
    this->x = x;
    this->y = y;
-  }
+  };
   Vector times(float s){
    this->x *= s;
    this->y *= s;
    return *this;
-  }
+  };
   Vector times(Vector v){
    this->x *= v.x;
    this->y *= v.y;
    return *this;
-  }
+  };
   Vector times(Vector* v){
    this->x *= v->x;
    this->y *= v->y;
    return *this;
-  }
+  };
   Vector add(Vector v){
    this->x += v.x;
    this->y += v.y;
    return *this;
-  }
+  };
   Vector add(Vector* v){
    this->x += v->x;
    this->y += v->y;
    return *this;
-  }
-  Vector* clone(){
-   return new Vector(this->x, this->y);
-  }
+  };
+  Vector clone(){
+   return Vector(this->x, this->y);
+  };
+  float dist(Vector v){
+   return sqrt(
+    pow2(
+     fabs(this->x - v.x)
+    )+
+    pow2(
+     fabs(this->y - v.y)
+    )
+   );
+  };
+  float dist(Vector* v){
+   return sqrt(
+    pow2(
+     fabs(this->x - v->x)
+    )+
+    pow2(
+     fabs(this->y - v->y)
+    )
+   );
+  };
+  float dir(Vector v){
+   return atan(
+    fabs(this->y - v.y)/
+    fabs(this->x - v.x)
+   );
+  };
+  float dir(Vector* v){
+   return atan(
+    fabs(this->y - v->y)/
+    fabs(this->x - v->x)
+   );
+  };
+  float abs(){
+   return sqrt(
+    pow2(this->x)+
+    pow2(this->y)
+   );
+  };
+  float angle(){
+   return atan(
+    this->y/
+    this->x
+   );
+  };
 };
 class VectorAngle: public Vector {
  public:
@@ -109,7 +156,7 @@ class Color {
   float G;
   float B;
   float A;
-  Color(float R, float G, float B, float A = .0f)
+  Color(float R, float G, float B, float A = 1.0f)
   {
    this->R = R;
    this->G = G;
@@ -191,6 +238,7 @@ class Particle {
     forceReaction();
     move();
     draw();
+    //play(http://youtu.be/GBHPowULV7k);
    }
    void setColor(Color* color){
     this->color = color;
@@ -200,14 +248,30 @@ class Particle {
 /* * * * * * *
  * main.cpp  *
  * * * * * * */
-Particle* p1 = new Particle(new Vector(-.5f,0),1,.5f,new Color(1,0,0));
+Particle* particles [5];
 
 void display(){
  glClearColor(0,0,0,1); //Černá
  glClear(GL_COLOR_BUFFER_BIT);
  
- p1->applyForce(new Vector(0.001f,0));
- p1->done();
+ int i = 0;
+ int j = 0;
+ do{
+  do{
+   if(i!=j){
+    particles[i]->applyForce(
+     new VectorAngle(
+      particles[i]->coord->dist(particles[j]->coord),
+      particles[i]->coord-> dir(particles[j]->coord)
+     )
+    );
+   }
+  }while(
+   ++j<sizeof(particles)
+  );
+ }while(
+  ++i<sizeof(particles)
+ );
  
  glFlush(); //Render now
 }
@@ -217,7 +281,16 @@ void timer(int value){
  glutTimerFunc(15,timer,0);
 }
 
-int main(int argc, char** argv){ 
+int main(int argc, char** argv){
+ int i = 0;
+ do{
+  particles[i] = new Particle(
+   new Vector(rand()%100/100.0f-0.5f,rand()%100/100.0f-0.5f),
+   1, 0.1f, new Color(1,0,0)
+  );
+ }while(
+  ++i<sizeof(particles)
+ );
  glutInit(&argc, argv);
  glutCreateWindow("Use the force, Luke");
  glutInitWindowSize(640,640);
